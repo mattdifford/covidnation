33fb5279ac3e63dae59582e79bea0fdedbce9b64
Matt Difford <matt@mrsavvy.co.uk>
2020-04-21 13:18:13 +0100

    New and improved

--- js/src/script.js
+++ js/src/script.js
@@ -53,7 +53,15 @@ $.fn.serializeObject = function () {
 function handleDataTable(url) {
     var $repeater = $('#table-repeater');
     $.get(url, function (data) {
-        data.forEach(function (element, index) {
+        $('.data-table__global-item').each(function () {
+            var value = data["Global"][$(this).attr("data-column")];
+            $(this).find("span").html(value.toLocaleString());
+        });
+        data["Countries"].forEach(function (element, index) {
+            if ($('.data-table__time span').html() === "") {
+                var date = new Date(element['Date']);
+                $('.data-table__time span').html(date.toLocaleString())
+            }
             var $new_row = $repeater.clone();
             $new_row.attr("id", "");
             $new_row.removeClass('data-table__row--template');
@@ -67,17 +75,12 @@ function handleDataTable(url) {
                         if (element[name] === "NA") {
                             $(this).html("No data");
                         } else {
-                            if ($(this).hasClass('data-table__column--datetime')) {
-                                var date = new Date(element[name]);
-                                $(this).html(date.toLocaleString());
-                            } else {
-                                $(this).html((element[name] ? element[name] : "No data"));
-                            }
+                            $(this).html((element[name] ? element[name] : "No data"));
                         }
                     }
                 } else {
                     if (name === "url") {
-                        $(this).html('<a class="button" href="/country/' + element["country"].toLowerCase().replace(" ", "-") + '">View more</a>')
+                        $(this).html('<a class="button" href="/country/' + element["Slug"] + '">View more</a>')
                     }
                 }
             });
@@ -87,40 +90,73 @@ function handleDataTable(url) {
     });
 }
 
-function handleCountryData(main_url) {
-    var current_country = window.location.pathname.split("/")[2];
+function handleCountryData(slug, title) {
+    var date = new Date();
+    date.setDate(date.getDate() - 1);
+    var url = "https://api.covid19api.com/live/country/" + slug + "/status/confirmed/date/" + date.toISOString();
     $('.data-dashboard').addClass('loading');
-    $.get(main_url, function (data) {
-        data.forEach(function (element, index) {
-            if (element["country"].toLowerCase().replace(" ", "-") === current_country) {
-                var details_url = element["moreData"];
-                $('.data-dashboard__title span').html(element["country"]);
-                var date = new Date(element["lastUpdatedApify"]);
-                $('.data-dashboard__time span').html(date.toLocaleString());
-                $('.data-dashboard__source span').html(element["sourceUrl"]);
-                $('.data-dashboard__button').attr("href", window.location.pathname + "-history");
-                $.get(details_url, function (data) {
-                    var ignore_columns = ["lastUpdatedAtApify", "readMe", "sourceUrl", "country", "lastUpdatedAtSource", "historyData"];
-                    for (var prop in data) {
-                        if (Object.prototype.hasOwnProperty.call(data, prop) && ignore_columns.indexOf(prop) == -1) {
-                            var $panel = $('#data_block_repeater').clone();
-                            $panel.attr("id", "");
-                            $panel.removeClass('data-block--template');
-                            var finalResult = camelCaseToSentenceCase(prop);
-                            $panel.find('.data-block__title').html(finalResult)
-                            var value = data[prop];
-                            if (typeof value === "number") {
-                                $panel.find('.data-block__value').html(value.toLocaleString());
+    $('.data-dashboard__time span').html(date.toLocaleString());
+    $('.data-dashboard__button').attr("href", "/country/" + slug + "-history");
+    var root = $('#data_block_group_repeater');
+    var totals = [];
+    $.get(url, function (data) {
+        var $repeater = $('#table-repeater');
+        if (data.length > 1) {
+            data.forEach(function (element) {
+                var $group = root.clone();
+                $group.attr("id", "");
+                $group.removeClass('data-dashboard__group--template');
+                $group.find('.data-dashboard__group-title').html((element["Province"] ? element["Province"] : element["City"]));
+                var $new_row = $repeater.clone();
+                $new_row.attr("id", "");
+                $new_row.removeClass('data-table__row--template');
+                var columns = $new_row.find('.data-table__column');
+                columns.each(function () {
+                    var name = $(this).attr("data-column");
+                    if (typeof element[name] !== "undefined") {
+                        if (typeof totals[$(this).attr("data-column")] === "undefined") {
+                            totals[$(this).attr("data-column")] = parseInt(element[name]);
+                        } else {
+                            totals[$(this).attr("data-column")] += parseInt(element[name]);
+                        }
+                        if (typeof element[name] == "number") {
+                            $(this).html(element[name].toLocaleString());
+                        } else {
+                            if (element[name] === "NA") {
+                                $(this).html("No data");
                             } else {
-                                $panel.find('.data-block__value').html(value);
+                                $(this).html((element[name] ? element[name] : "No data"));
                             }
-                            $('#data_block_repeater').after($panel);
+                        }
+                    } else {
+                        if (name === "Province/City") {
+                            var title;
+                            if (element["Province"]) {
+                                title = element["Province"];
+                            } else if (element["City"]) {
+                                title = element["City"];
+                            } else {
+                                title= element["Country"]
+                            }
+                            $(this).html(title)
                         }
                     }
-                    $('.data-dashboard').removeClass('loading');
                 });
-            }
-        });
+                $repeater.before($new_row);
+            });
+            $('.data-dashboard__group--total .data-block').each(function () {
+                var value = totals[$(this).attr("data-column")];
+                $(this).find('.data-block__value').html(value.toLocaleString());
+            });
+        } else {
+            $('.data-table').hide();
+            $('.data-dashboard__group--total .data-block').each(function () {
+                var value = data[0][$(this).attr("data-column")];
+                $(this).find('.data-block__value').html(value.toLocaleString());
+            });
+        }
+
+        $('.data-dashboard').removeClass('loading');
     });
 }
 
@@ -130,87 +166,116 @@ function camelCaseToSentenceCase(text) {
 }
 
 function handleCountryHistoryData(main_url) {
-    var current_country = window.location.pathname.split("/")[2].replace("-history", "");
-    $('.data-dashboard').addClass('loading');
-    $.get(main_url, function (data) {
-        data.forEach(function (element, index) {
-            if (element["country"].toLowerCase().replace(" ", "-") === current_country) {
-                var details_url = element["historyData"];
-                $('.data-dashboard__title span').html(element["country"]);
-                var date = new Date(element["lastUpdatedApify"]);
-                $('.data-dashboard__time span').html(date.toLocaleString());
-                $('.data-dashboard__source span').html(element["sourceUrl"]);
-                var datasets = [];
-                $.get(details_url, function (data) {
-                    var ignore_columns = ["lastUpdatedAtApify", "readMe", "sourceUrl", "country", "lastUpdatedAtSource", "historyData", "test"];
-                    for (var row in data) {
-                        var date = new Date(data[row]["lastUpdatedAtApify"]);
-                        var date_id = date.getFullYear() + "-" + (parseInt(date.getMonth()) + 1) + "-" + date.getDate();
-                        for (var prop in data[row]) {
-                            //Manual corrections to property name. Not ideal, but necessary to consolidate field names which have changed over time
-                            var corrected_prop = prop.replace("scottland", "scotland");
-                            corrected_prop = corrected_prop.replace("Confirmed", "")
-                            if (corrected_prop === "ireland") {
-                                corrected_prop = "northernIreland";
-                            }
-                            //End of property corrections
-                            if (Object.prototype.hasOwnProperty.call(data[row], corrected_prop) && ignore_columns.indexOf(corrected_prop) == -1) {
-                                var value = data[row][corrected_prop];
-                                if (typeof value != "number") {
-                                    value = 0;
-                                }
-                                if (typeof datasets[corrected_prop] !== "undefined") {
-                                    if (typeof datasets[corrected_prop][date_id] !== "undefined") {
-                                        if (value > datasets[corrected_prop][date_id]) {
-                                            datasets[corrected_prop][date_id] = value;
-                                        }
-                                    } else {
-                                        datasets[corrected_prop][date_id] = value;
-                                    }
-                                } else {
-                                    datasets[corrected_prop] = [];
-                                    datasets[corrected_prop][date_id] = value;
-                                }
-                            }
-                        }
-                    }
-                    for (var dataset in datasets) {
-                        var $template = $('#data_chart_repeater');
-                        var $chart = $template.clone();
-                        $chart.removeClass('data-chart--template')
-                        var id = "data_chart_" + dataset;
-                        $chart.attr("id", "");
-                        var $canvas = $chart.find("canvas");
-                        $canvas.attr("id", id);
-                        $template.before($chart);
-                        var ctx = $canvas;
-                        var formatted_data = [];
-                        for (var data_row in datasets[dataset]) {
-                            var date = new Date(data_row);
-                            formatted_data.push({ x: date, y: parseInt(datasets[dataset][data_row]) });
-                        }
-                        var chart = new Chart(ctx, {
-                            type: 'line',
-                            data: formatted_data,
-                            options: {
-                                title: {
-                                    display: true,
-                                    text: camelCaseToSentenceCase(dataset)
-                                },
-                                scales: {
-                                    xAxes: [{
-                                        type: 'time',
-                                        distribution: 'series',
-                                    }]
-                                }
-                            }
-                        });
+    // var current_country = window.location.pathname.split("/")[2].replace("-history", "");
+    // $('.data-dashboard').addClass('loading');
+    // $.get(main_url, function (data) {
+    //     data.forEach(function (element, index) {
+    //         if (element["country"].toLowerCase().replace(" ", "-") === current_country) {
+    //             var details_url = element["historyData"];
+    //             $('.data-dashboard__title span').html(element["country"]);
+    //             var date = new Date(element["lastUpdatedApify"]);
+    //             $('.data-dashboard__time span').html(date.toLocaleString());
+    //             $('.data-dashboard__source span').html(element["sourceUrl"]);
+    //             var datasets = [];
+    //             $.get(details_url, function (data) {
+    //                 var ignore_columns = ["lastUpdatedAtApify", "readMe", "sourceUrl", "country", "lastUpdatedAtSource", "historyData", "test"];
+    //                 for (var row in data) {
+    //                     var date = new Date(data[row]["lastUpdatedAtApify"]);
+    //                     var date_id = date.getFullYear() + "-" + pad(parseInt(date.getMonth() + 1), 2) + "-" + pad(date.getDate(), 2);
+    //                     for (var prop in data[row]) {
+    //                         //Manual corrections to property name. Not ideal, but necessary to consolidate field names which have changed over time
+    //                         var corrected_prop = prop.replace("scottland", "scotland");
+    //                         corrected_prop = corrected_prop.replace("Confirmed", "");
+    //                         if (corrected_prop === "infected") {
+    //                             corrected_prop = "totalInfected";
+    //                         }
+    //                         if (corrected_prop === "ireland") {
+    //                             corrected_prop = "northernIreland";
+    //                         }
+    //                         //End of property corrections
+    //                         if (Object.prototype.hasOwnProperty.call(data[row], corrected_prop) && ignore_columns.indexOf(corrected_prop) == -1) {
+    //                             var value = data[row][corrected_prop];
+    //                             if (typeof value != "number") {
+    //                                 value = 0;
+    //                             }
+    //                             if (typeof datasets[corrected_prop] !== "undefined") {
+    //                                 if (typeof datasets[corrected_prop][date_id] !== "undefined") {
+    //                                     if (value > datasets[corrected_prop][date_id]) {
+    //                                         datasets[corrected_prop][date_id] = value;
+    //                                     }
+    //                                 } else {
+    //                                     datasets[corrected_prop][date_id] = value;
+    //                                 }
+    //                             } else {
+    //                                 datasets[corrected_prop] = [];
+    //                                 datasets[corrected_prop][date_id] = value;
+    //                             }
+    //                         }
+    //                     }
+    //                 }
+    //                 for (var dataset in datasets) {
+    //                     var $template = $('#data_chart_repeater');
+    //                     var $chart = $template.clone();
+    //                     $chart.removeClass('data-chart--template')
+    //                     var id = "data_chart_" + dataset;
+    //                     $chart.attr("id", "");
+    //                     $template.before($chart);
+    //                     var data = new google.visualization.DataTable();
 
-                    }
-                    $('.data-dashboard').removeClass('loading');
-                });
+    //                     data.addColumn('date', "Date");
+    //                     data.addColumn('number', camelCaseToSentenceCase(dataset));
+    //                     var total = 0;
+    //                     for (var data_row in datasets[dataset]) {
+    //                         total += parseInt(datasets[dataset][data_row]);
+    //                         data.addRows([
+    //                             [new Date(data_row), parseInt(datasets[dataset][data_row])]
+    //                         ]);
+    //                     }
+    //                     var materialOptions = {
+    //                         backgroundColor: '#1D1D1D',
+    //                         colors: ['#1e88e5'],
+    //                         fontName: 'objektiv-mk2, sans-serif',
+    //                         height: 500,
+    //                         vAxis: {
+    //                             textStyle: {
+    //                                 color: '#fff'
+    //                             }
+    //                         },
+    //                         hAxis: {
+    //                             textStyle: {
+    //                                 color: '#fff'
+    //                             }
+    //                         },
+    //                         legend: {
+    //                             textStyle: {
+    //                                 color: '#fff'
+    //                             }
+    //                         },
+    //                         titleTextStyle: {
+    //                             fontSize: 20,
+    //                             color: '#fff',
+    //                             bold: true,
+    //                         },
+    //                         chart: {
+    //                             title: camelCaseToSentenceCase(dataset)
+    //                         },
+    //                     };
+    //                     if (total > 0) {
 
-            }
-        });
-    });
+    //                     }
+    //                     var chart = new google.charts.Line($chart[0]);
+    //                     chart.draw(data, google.charts.Line.convertOptions(materialOptions));
+    //                 }
+    //                 $('.data-dashboard').removeClass('loading');
+    //             });
+
+    //         }
+    //     });
+    // });
+}
+
+function pad(n, width, z) {
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
 }
